a4fcdafb18afc8bc440966cd5d9f5e5f76ec10cb
Pyetra <pyetraquinquio2007@gmail.com>
2024-04-16 14:38:20 -0300

    path e fs

--- ativ_dirs/index.js
+++ ativ_dirs/index.js
@@ -1,7 +1,10 @@
 const path = require("path");
-
-
 const fs = require('fs')
+
+
+const antiga = path.join('./arquivos_desorganizados');
+const nova = path.join('./arquivos_organizados')
+
 
 if (!fs.existsSync('./arquivos_organizados')) {
     fs.mkdirSync('arquivos_organizados')
@@ -13,4 +16,44 @@ if (!fs.existsSync('./arquivos_organizados')) {
     fs.mkdirSync('arquivos_organizados/outros')
 }
 
-if
+async function moveFile(antiga, nova) {
+    try {
+        await fs.promises.rename(antiga, nova);
+        console.log(`Movido de ${antiga} para ${nova}`);
+    } catch (error) {
+        console.error("Erro ao mover o arquivo:", error);
+    }
+}
+
+fs.readdir(antiga, (err,files) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+files.forEach(file => {
+    const tipo = path.extname(file);
+
+    if(tipo === '.pdf') {
+        moveFile(path.join(antiga, file), path.join(nova, 'documentos', file));
+    }
+
+    if(tipo === '.txt') {
+        moveFile(path.join(antiga, file), path.join(nova, 'documentos', file));
+    }
+
+
+    if(tipo === '.PNG') {
+        moveFile(path.join(antiga, file), path.join(nova, 'imagens', file));
+    }
+
+    if(tipo === '.html') {
+        moveFile(path.join(antiga, file), path.join(nova, 'outros', file));
+    }
+
+    if(tipo === '.sql') {
+        moveFile(path.join(antiga, file), path.join(nova, 'outros', file));
+    }
+
+})
+})
